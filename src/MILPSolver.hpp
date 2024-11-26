09de8cd04f4d4f9c34beadc9968681c72baebda6
Leon Casas <leonn.casass@gmail.com>
2024-11-25 21:38:56 -0600

    - Add TSP solvers already created and teached by @VincentALBoyer - The MILPSolver here implemented use MTZ to solve the TSP

---
+++ src/MILPSolver.hpp
@@ -0,0 +1,21 @@
+#include "Solver.hpp"
+
+class MILPSolver : public Solver
+{
+
+    IloEnv _env;
+    IloModel _model;
+    IloCplex _cplex;
+    IloArray<IloNumVarArray> _x;
+    IloNumVarArray _u;
+
+    void solvemethod(Solution *S);
+
+public:
+    MILPSolver(Instance *I);
+    ~MILPSolver();
+
+    double gap() { return _cplex.getMIPRelativeGap(); };
+
+    Solution *recoversolution();
+};
